7c6df8c38501bcd6e15927688e6357b46df49221
tryrone <tegararuvwe@gmail.com>
2019-06-15 20:18:50 +0100

    all parts concluded

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const schema = require("./schema/schema");
+const cors = require("cors");
 
 const app = express();
+
+//Allow cors origin
+app.use(cors());
 
 app.use(
   "/graphql",
